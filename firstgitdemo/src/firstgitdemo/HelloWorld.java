f77d3c8776b042a2fce7334ef5f0022ab289e498
raviteja <raviteja1986@gmail.com>
2014-11-29 12:33:42 +0530

    2 nd mod

--- firstgitdemo/src/firstgitdemo/HelloWorld.java
+++ firstgitdemo/src/firstgitdemo/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("========55555555555555555===============");
+		System.out.println("ravi teja katari");
 
 	}
 
